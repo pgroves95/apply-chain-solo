71e2dd1f09e26b882ec7739a135f16dd900d8051
Patrick Groves <patrickalexgroves@gmail.com>
2021-06-08 12:38:26 -0600

    working redux cycle

---
+++ app-dir/src/actions/applyFormActions.js
@@ -0,0 +1,15 @@
+import {FIRST_NAME_INPUT,
+    LAST_NAME_INPUT,
+    EMAIL_INPUT,
+    PHONE_INPUT,
+    EXTENDED_INPUT_1,
+    EXTENDED_INPUT_2,
+    SUBMIT_FORM
+    } from './action-types'
+
+const handleInputChange = (action, payload) {
+    switch(action.type){
+        case FIRST_NAME_INPUT:
+            
+    }
+}
