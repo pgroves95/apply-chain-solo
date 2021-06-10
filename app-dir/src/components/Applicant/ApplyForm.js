2671d2458f715f9aad0a9cb9d9a04351826d9a29
Patrick Groves <patrickalexgroves@gmail.com>
2021-06-09 22:32:13 -0600

    working towards adding app component

--- app-dir/src/components/Applicant/ApplyForm.js
+++ app-dir/src/components/Applicant/ApplyForm.js
@@ -1,17 +1,31 @@
 import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
 import '../../styles/ApplyForm.css'
+import {
+        handleFirstName,
+        handleLastName,
+        handleEmail,
+        handlePhone,
+        handleExt1,
+        handleExt2
+    } from '../../actions/applyFormActions'
+import { handleInputChange } from '../../action'
+import { addSubmittedApp } from '../../actions/viewApplicationsActions'
 
 function ApplyForm() {
+    const state = useSelector(state=>state)
+    
+    const dispatch = useDispatch()
     return (
         <div className="apply-form">
         <h1 className="job-header">Apply for the SWE I position at Lunar Unicorn</h1>
-            <input className="form-field" type="text" placeholder="first name"/>
-            <input className="form-field" type="text" placeholder="last name"/>
-            <input className="form-field" type="text" placeholder="email" />
-            <input className="form-field" type="text" placeholder="mobile number" />
-            <textarea className="form-field" name="q1" id="" cols="30" rows="10" placeholder="Tell us about a time when you showed entrepreneurship in an non-enterpreneurial situation."></textarea>
-            <textarea className="form-field" name="q1" id="" cols="30" rows="10" placeholder="If you had to choose between saving your relationships or sending this company to the moon, tell us which one you would pick and why."></textarea>
-            <button>Shoot for the Moon</button>
+            <input className="form-field" type="text" placeholder="first name" onChange={(e)=>handleFirstName(dispatch, e.target.value)}/>
+            <input className="form-field" type="text" placeholder="last name" onChange={(e)=>handleLastName(dispatch, e.target.value)}/>
+            <input className="form-field" type="text" placeholder="email" onChange={(e)=>handleEmail(dispatch, e.target.value)}/>
+            <input className="form-field" type="text" placeholder="mobile number" onChange={(e)=>handlePhone(dispatch, e.target.value)}/>
+            <textarea className="form-field" name="q1" id="" cols="30" rows="10" onChange={(e)=>handleExt1(dispatch, e.target.value)} placeholder="Tell us about a time when you showed entrepreneurship in an non-enterpreneurial situation."></textarea>
+            <textarea className="form-field" name="q1" id="" cols="30" rows="10" onChange={(e)=>handleExt2(dispatch, e.target.value)} placeholder="If you had to choose between saving your relationships or sending this company to the moon, tell us which one you would pick and why."></textarea>
+            <button onClick={()=>addSubmittedApp(dispatch, state.handle)}>Shoot for the Moon</button>
         </div>
     )
 }
